811748881fd625f4e8a9b6348f2e7de9f848b022
ebonbu <ebonbu.s.l@gmail.com>
2020-05-26 18:53:57 +0200

    second

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // 이차원 배열 구조
+    //    Tile[,] m_TilesArray = new Tile[6, 6];
+
+    // 사전 구조<key, value>
+    Dictionary<string,Tile> m_TileDictionary = new Dictionary<string, Tile>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tilePrefab = Resources.Load("Prefabs/CandyPurple") as GameObject;
+        Tile tile_0 = Instantiate<Tile>(tilePrefab.transform.GetComponent<Tile>());
+        
+        tile_0.transform.position = Vector3.zero;
+
+        //타일의 부모를 Board로 설정해야 보임
+        tile_0.transform.SetParent(this.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        transform.DOPunchScale(Vector3.one, 1f);
+    }
+}
